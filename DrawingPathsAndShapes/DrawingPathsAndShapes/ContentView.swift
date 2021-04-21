7215ee715bb338c54cbf5f08fbae2f63d2668dc2
Thongchai Subsaidee <chanoktrue@me.com>
2021-04-21 11:53:13 +0700

    Finish project

--- DrawingPathsAndShapes/DrawingPathsAndShapes/ContentView.swift
+++ DrawingPathsAndShapes/DrawingPathsAndShapes/ContentView.swift
@@ -9,9 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            Path { path in
+                let width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95,
+                        y: height * (0.20 + HexagonParameters.adjustment)
+                    )
+                )
+                
+                HexagonParameters.segments.forEach { segment in
+                    path.addLine(
+                        to: CGPoint(
+                            x: width * segment.line.x,
+                            y: width * segment.line.y
+                        )
+                    )
+                    
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x,
+                            y: width * segment.curve.y
+                        ),
+                        control: CGPoint(
+                            x: width * segment.control.x,
+                            y: height * segment.control.y
+                        )
+                    )
+                    
+                }
+            
+            }
+            .fill(Color.black)
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
