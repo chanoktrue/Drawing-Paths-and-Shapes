03a78c4012b658dab8f5fa744f1163883a2c9a1a
Thongchai Subsaidee <chanoktrue@me.com>
2021-04-21 11:32:09 +0700

    First project

---
+++ DrawingPathsAndShapes/DrawingPathsAndShapes/DrawingPathsAndShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingPathsAndShapesApp.swift
+//  DrawingPathsAndShapes
+//
+//  Created by Thongchai Subsaidee on 21/4/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct DrawingPathsAndShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
